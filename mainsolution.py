9d1bd826dcea65db5dd521ecde63e051628c5ae4
yoijungwon <rachelseow2006@gmail.com>
2023-08-06 23:54:31 +0800

    all comments written, slight change for main sltn

--- mainsolution.py
+++ mainsolution.py
@@ -1,16 +1,13 @@
-import overheads, cash_on_hand, profit_and_loss
+import overheads
+import cash_on_hand
+import profit_and_loss
+#import the three python file using modularisation
 
+#call the function
+overheads.highest_oh()
+cash_on_hand.cash_deficit()
+profit_and_loss.profit_deficit()
 
-def main():
-    """
-    Coordinate and execute the different functions from the three different files.
-    Paraneters are not required.
-    """
-    overheads.highest_oh()
-    cash_on_hand.cash_deficit()
-    profit_and_loss.profit_deficit()
-
-main() #call function
     
 
 
