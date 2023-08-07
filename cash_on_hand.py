5e23ed12fe84c0042ba67e2235e40675eb943ccc
yoijungwon <rachelseow2006@gmail.com>
2023-08-07 15:51:56 +0800

    code modified

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -20,7 +20,6 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
 
 # print(cash_on_hand)
 
-
 def cash_deficit():
     """
     Find all the days which has cash deficit happened, and the amount.
@@ -51,59 +50,13 @@ def cash_deficit():
     for cash in cash_diff_list:
         if cash < 0:
             cash_deficit_list.append(cash) #append all the negative value from cash_diff_list into cash_deficit_list
-            index_list.append(cash_diff_list.index(cash)) #append the index of the negative values 
+            index_list.append(cash_diff_list.index(cash)) #append the index of the negative values
+        
        
     summary_report = "" #create an empty string
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[0]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[0]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[1]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[1]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[2]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[2]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[3]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[3]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[4]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[4]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[5]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[5]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[6]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[6]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[7]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[7]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[8]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[8]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[9]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[9]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[10]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[10]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[11]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[11]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[12]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[12]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[13]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[13]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[14]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[14]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[15]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[15]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[16]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[16]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[17]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[17]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[18]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[18]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[19]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[19]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[20]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[20]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[21]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[21]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[22]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[22]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[23]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[23]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[24]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[24]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[25]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[25]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[26]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[26]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[27]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[27]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[28]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[28]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[29]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[29]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[30]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[30]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[31]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[31]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[32]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[32]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[33]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[33]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[34]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[34]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[35]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[35]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[36]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[36]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[37]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[37]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[38]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[38]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[39]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[39]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[40]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[40]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[41]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[41]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[42]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[42]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[43]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[43]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[44]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[44]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[45]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[45]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[46]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[46]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[47]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[47]).replace("-", "")
-    summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[48]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[48]).replace("-", "")
-
+    for i in range(len(index_list)):
+        summary_report +=  "\n" + "[CASH DEFICIT] DAY: " + str(index_list[i]) + ", AMOUNT: " + "USD" + str(cash_deficit_list[i]).replace("-", "")
+   
     with open("summary_report.txt", "a") as file:
         file.write(summary_report)
 
--- profit_and_loss.py
+++ profit_and_loss.py
@@ -18,7 +18,7 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #and append to profit_and_loss list
         profit_and_loss.append([row[0], row[4]])
 
-print(profit_and_loss)
+#print(profit_and_loss)
 
 
 def profit_deficit():
@@ -50,63 +50,11 @@ def profit_deficit():
         if amount < 0:
             day_amount_list.append(amount) #append all the negative value from diff_list into day_amount_list
             amount_index.append(diff_list.index(amount)) #append the index of the negative values 
-    
-
-        
    
     summary_report_pl = ""  #create an empty string
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[0]) + ", AMOUNT: " + "USD" + str(day_amount_list[0]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[1]) + ", AMOUNT: " + "USD" + str(day_amount_list[1]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[2]) + ", AMOUNT: " + "USD" + str(day_amount_list[2]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[3]) + ", AMOUNT: " + "USD" + str(day_amount_list[3]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[4]) + ", AMOUNT: " + "USD" + str(day_amount_list[4]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[5]) + ", AMOUNT: " + "USD" + str(day_amount_list[5]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[6]) + ", AMOUNT: " + "USD" + str(day_amount_list[6]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[7]) + ", AMOUNT: " + "USD" + str(day_amount_list[7]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[8]) + ", AMOUNT: " + "USD" + str(day_amount_list[8]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[9]) + ", AMOUNT: " + "USD" + str(day_amount_list[9]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[10]) + ", AMOUNT: " + "USD" + str(day_amount_list[10]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[11]) + ", AMOUNT: " + "USD" + str(day_amount_list[11]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[12]) + ", AMOUNT: " + "USD" + str(day_amount_list[12]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[13]) + ", AMOUNT: " + "USD" + str(day_amount_list[13]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[14]) + ", AMOUNT: " + "USD" + str(day_amount_list[14]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[15]) + ", AMOUNT: " + "USD" + str(day_amount_list[15]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[16]) + ", AMOUNT: " + "USD" + str(day_amount_list[16]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[17]) + ", AMOUNT: " + "USD" + str(day_amount_list[17]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[18]) + ", AMOUNT: " + "USD" + str(day_amount_list[18]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[19]) + ", AMOUNT: " + "USD" + str(day_amount_list[19]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[20]) + ", AMOUNT: " + "USD" + str(day_amount_list[20]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[21]) + ", AMOUNT: " + "USD" + str(day_amount_list[21]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[22]) + ", AMOUNT: " + "USD" + str(day_amount_list[22]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[23]) + ", AMOUNT: " + "USD" + str(day_amount_list[23]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[24]) + ", AMOUNT: " + "USD" + str(day_amount_list[24]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[25]) + ", AMOUNT: " + "USD" + str(day_amount_list[25]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[26]) + ", AMOUNT: " + "USD" + str(day_amount_list[26]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[27]) + ", AMOUNT: " + "USD" + str(day_amount_list[27]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[28]) + ", AMOUNT: " + "USD" + str(day_amount_list[28]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[29]) + ", AMOUNT: " + "USD" + str(day_amount_list[29]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[30]) + ", AMOUNT: " + "USD" + str(day_amount_list[30]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[31]) + ", AMOUNT: " + "USD" + str(day_amount_list[31]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[32]) + ", AMOUNT: " + "USD" + str(day_amount_list[32]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[33]) + ", AMOUNT: " + "USD" + str(day_amount_list[33]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[34]) + ", AMOUNT: " + "USD" + str(day_amount_list[34]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[35]) + ", AMOUNT: " + "USD" + str(day_amount_list[35]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[36]) + ", AMOUNT: " + "USD" + str(day_amount_list[36]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[37]) + ", AMOUNT: " + "USD" + str(day_amount_list[37]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[38]) + ", AMOUNT: " + "USD" + str(day_amount_list[38]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[39]) + ", AMOUNT: " + "USD" + str(day_amount_list[39]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[40]) + ", AMOUNT: " + "USD" + str(day_amount_list[40]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[41]) + ", AMOUNT: " + "USD" + str(day_amount_list[41]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[42]) + ", AMOUNT: " + "USD" + str(day_amount_list[42]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[43]) + ", AMOUNT: " + "USD" + str(day_amount_list[43]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[44]) + ", AMOUNT: " + "USD" + str(day_amount_list[44]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[45]) + ", AMOUNT: " + "USD" + str(day_amount_list[45]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[46]) + ", AMOUNT: " + "USD" + str(day_amount_list[46]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[47]) + ", AMOUNT: " + "USD" + str(day_amount_list[47]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[48]) + ", AMOUNT: " + "USD" + str(day_amount_list[48]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[49]) + ", AMOUNT: " + "USD" + str(day_amount_list[49]).replace("-", "")
-    summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[50]) + ", AMOUNT: " + "USD" + str(day_amount_list[50]).replace("-", "")
-
+    for i in range(len(amount_index)):
+        summary_report_pl +=  "\n" + "[PROFIT DEFICIT] DAY: " + str(amount_index[i]) + ", AMOUNT: " + "USD" + str(day_amount_list[i]).replace("-", "")
+    
     with open("summary_report.txt", "a") as file:
         file.write(summary_report_pl)
 
