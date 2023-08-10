9526e7dc93e03bf43a38d19e4b7d6bd8131fc133
JamaineLim1 <s10258485@connect.np.edu.sg>
2023-08-10 13:32:36 +0800

    main done by jamaine

--- mainsolution.py
+++ mainsolution.py
@@ -3,7 +3,7 @@ import cash_on_hand
 import profit_and_loss
 #import the three python file using modularisation
 
-#call the function
+#execute the files by calling the three functions
 overheads.highest_oh()
 cash_on_hand.cash_deficit()
 profit_and_loss.profit_deficit()
