39599dae38af00e74158cf7581149bad6060e518
username <emailaddress>
2023-08-10 12:57:13 +0800

    main done by jamaine

--- mainsolution.py
+++ mainsolution.py
@@ -1,9 +1,9 @@
 import overheads
 import cash_on_hand
 import profit_and_loss
-#import the three python file using modularisation
+#import the three python file 
 
-#call the function
+#execute the three files by calling the function
 overheads.highest_oh()
 cash_on_hand.cash_deficit()
 profit_and_loss.profit_deficit()
